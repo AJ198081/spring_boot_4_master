07ad173024e01f403e63192d61112431fcfc6ee5
abhandal <amarjit.bhandal@superchoice.com.au>
2026-03-27 07:42:30 +1100

    Add default error handler to RestClient in TestConfig

--- accounts/src/test/java/dev/aj/accounts/setup/TestConfig.java
+++ accounts/src/test/java/dev/aj/accounts/setup/TestConfig.java
@@ -3,6 +3,8 @@ package dev.aj.accounts.setup;
 import net.datafaker.Faker;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClient;
 
 import java.util.Locale;
@@ -13,6 +15,9 @@ public class TestConfig {
     public RestClient restClient(String baseUrl) {
         return RestClient.builder()
                 .baseUrl(baseUrl)
+                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
+                    throw new HttpStatusCodeException(response.getStatusCode(), "Error calling %s".formatted(request.getURI())) {};
+                })
                 .build();
     }
 
